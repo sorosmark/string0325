568aebd74718764e57e26c53ff7eb77873c006af
sorosmark <soros.mark@diak.szbi-pg.hu>
2025-03-25 10:40:11 +0100

    string

---
+++ 01_string.js
@@ -0,0 +1,32 @@
+console.log("Hello");
+
+let s = "karakterlanc \"Hello\"";
+console.log(s);
+
+let ujsor = "karakterlanc \n \"Hello\"";
+console.log(ujsor);
+console.log(ujsor.length);
+
+let sz = "JavaScript";
+for (let index = 0; index < sz.length; index++) 
+    {
+        console.log(sz[index]);
+    }
+
+let sz1 = "MySQL";
+for (betu of sz1) 
+    {
+        console.log(betu);
+    }
+
+    console.log(sz + sz1);
+    console.log(sz + " " + sz1);
+    console.log(`${sz} ${sz1}`);
+
+    let szovegosszefuzes = sz.concat(" ",  sz1);
+    console.log(szovegosszefuzes);
+    
+    console.log(sz1.charAt(4));
+    console.log(sz1.charCodeAt(4));
+
+console.log(sz1.startsWith("M"));
